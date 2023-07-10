5bb5947c16bda6d89f0b4fc8a63351c78ea3ca2e
delahocheemmanuel <delahochemanu@hotmail.fr>
2023-07-10 14:06:46 +0200

    structuration  en cours

--- backend/app.js
+++ backend/app.js
@@ -8,22 +8,28 @@ mongoose
   .connect(mongoDBURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Connexion à MongoDB réussie !');
-    // Votre code pour interagir avec la base de données MongoDB ici
+    
   })
   .catch((error) => {
     console.log('Connexion à MongoDB échouée !', error);
   });
 
+  app.use(express.json())
+  
+  app.use((req, res, next) => {
+      res.setHeader('Access-Control-Allow-Origin', '*')
+      res.setHeader(
+          'Access-Control-Allow-Headers',
+          'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+      )
+      res.setHeader(
+          'Access-Control-Allow-Methods',
+          'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+      )
+      next()
+  })  
+
 module.exports = app;
 
 
 
-mongoose
-    .connect(
-        'mongodb+srv://:delahochemanuHUwo3jHeyWzAsFni@book-pme76.mongodb.net/test?retryWrites=true&w=majority',
-        { useNewUrlParser: true, useUnifiedTopology: true }
-    )
-    .then(() => console.log('Connexion à MongoDB réussie !'))
-    .catch(() => console.log('Connexion à MongoDB échouée !'));
-
-module.exports = app;
---
+++ backend/controllers/user.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const app = express();
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const mongoose = require('mongoose');
