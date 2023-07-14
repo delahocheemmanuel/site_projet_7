d424888ad5e0724b8d6bcc7c30cf17934dcad926
delahocheemmanuel <delahochemanu@hotmail.fr>
2023-07-14 23:06:50 +0200

    delelte dossier backend et frontend dans frontend

--- frontend/backend/routes/user.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userCtrl = require('../controllers/user');
-
-router.post('/signup',userCtrl.signup) // Inscription
-router.post('/login',userCtrl.login) // Connexion
-
-module.exports = router; // 
