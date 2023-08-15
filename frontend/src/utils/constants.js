23192973ff508c7440adbdcd38568a07fcbb7644
delahocheemmanuel <delahochemanu@hotmail.fr>
2023-08-15 18:03:16 +0200

    constant.js chemin +/

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://mon-vieux-grimoire-ojmb.onrender.com';
+const API_URL = 'https://mon-vieux-grimoire-ojmb.onrender.com/';
 export const API_ROUTES = {
   SIGN_UP: `${API_URL}/api/auth/signup`,
   SIGN_IN: `${API_URL}/api/auth/login`,
