bf4cb671c8a67bfb5d2c63665eb1ad3a0111ef38
delahocheemmanuel <delahochemanu@hotmail.fr>
2023-08-15 18:06:49 +0200

    modif server.js

--- backend/server.js
+++ backend/server.js
@@ -1,62 +1,56 @@
-// Importer le module HTTP pour créer le serveur HTTP
-const https = require("https");
-// Importer l'application Express créée dans le fichier "app.js"
-const app = require("./app");
+// Charger les variables d'environnement depuis le fichier .env
+require('dotenv').config();
+
+// Importer les modules nécessaires
+const http = require('http');
+
+const app = require('./app'); // Assurez-vous que le chemin vers votre fichier Express.js est correct
 
 // Fonction pour normaliser le numéro de port
 const normalizePort = (val) => {
-  const port = parseInt(val, 10); // Convertir le numéro de port en entier décimal
-
-  if (isNaN(port)) {
-    // Si le port n'est pas un nombre, le retourner tel quel
-    return val;
-  }
-  if (port >= 0) {
-    // Si le port est un nombre positif, le retourner
-    return port;
-  }
-  // Sinon, retourner faux (port invalide)
-  return false;
+    const port = parseInt(val, 10);
+    if (isNaN(port)) {
+        return val;
+    }
+    if (port >= 0) {
+        return port;
+    }
+    return false;
 };
 
 // Récupérer le numéro de port depuis les variables d'environnement ou utiliser le port 4000 par défaut
-const port = normalizePort(process.env.PORT || "4000");
-// Définir le numéro de port dans l'application Express
-app.set("port", port);
-
-// Fonction de gestion des erreurs liées au serveur
-const errorHandler = (error) => {
-  if (error.syscall !== "listen") {
-    throw error;
-  }
-  const address = server.address();
-  const bind =
-    typeof address === "string" ? "pipe " + address : "port: " + port;
-  switch (error.code) {
-    case "EACCES":
-      console.error(bind + " requires elevated privileges.");
-      process.exit(1);
-      break;
-    case "EADDRINUSE":
-      console.error(bind + " is already in use.");
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-};
+const port = normalizePort(process.env.PORT || '4000');
+app.set('port', port);
 
 // Créer le serveur HTTP en utilisant l'application Express
-const server = https.createServer(app);
+const server = http.createServer(app);
 
 // Gérer les erreurs liées au serveur en utilisant la fonction errorHandler
-server.on("error", errorHandler);
+const errorHandler = (error) => {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+    const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+};
+server.on('error', errorHandler);
 
 // Gérer l'événement de mise en écoute du serveur (lorsqu'il démarre)
-server.on("listening", () => {
-  const address = server.address();
-  const bind = typeof address === "string" ? "pipe " + address : "port " + port;
-  console.log("Listening on " + bind); // Afficher un message dans la console indiquant que le serveur écoute sur un certain port ou adresse
+server.on('listening', () => {
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + address.port;
+    console.log('Listening on ' + bind);
 });
 
 // Faire écouter le serveur sur le numéro de port spécifié
